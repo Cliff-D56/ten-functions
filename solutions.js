064301cd7e458a720066578963e8db4f1edd6428
Clifford Gardner <cliffordgardner9@gmail.com>
2023-08-01 15:17:16 -0500

    Finished 10 functions

--- solutions.js
+++ solutions.js
@@ -1,5 +1,54 @@
 "use strict";
-
+function isTrue(x){
+    return x===true;
+}
+function isFalse(x){
+    return x===false;
+}
+function not(x){
+    return !x;
+}
+function addOne(x){
+    if (isNaN(x)){
+      return NaN;
+    }
+    else if(x===Infinity){
+        return x + 1;
+    }
+    else{
+        return parseInt(x) + 1;
+    }
+    //return isNaN(x) ? NaN: parseInt(x) + 1;
+}
+function isEven(x) {
+    if (isNaN(parseInt(x))){
+        return false;
+    }
+    else{
+        return x%2===0;
+    }
+    // else if(typeof parseInt(x)=="number"){
+    //     return x%2===0;
+    // }
+    // else{
+    //     return false;
+    // }
+}
+function isIdentical(x,y){
+    return x===y;
+}
+function isEqual(x,y){
+    return x == y;
+}
+function or(x,y){
+    return x || y;
+}
+function and(x,y){
+    return x && y;
+}
+function concat(x,y){
+    return x.toString()+y.toString();
+}
 /**
  * Write your solutions here.
  *
